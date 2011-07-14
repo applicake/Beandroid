c328c7087abbce9a39c3c5779179d4c2f5c7ca67
Bartek Filipowicz <bartek.f@applicake.com>
2011-07-14 17:04:34 +0200

    modifies ServerEnvironment class in order to partly implement parcelable interface

--- Beanstalk Android Client/src/com/applicake/beanstalkclient/ServerEnvironment.java
+++ Beanstalk Android Client/src/com/applicake/beanstalkclient/ServerEnvironment.java
@@ -12,17 +12,19 @@
   </server-environment>
  */
 
-
 package com.applicake.beanstalkclient;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-public class ServerEnvironment {
-  
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ServerEnvironment implements Parcelable {
+
   static SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-  
+
   // basic server environment data
   private int accountId;
   private boolean automatic;
@@ -33,86 +35,142 @@ public class ServerEnvironment {
   private String name;
   private int repositoryId;
   private long updatedAt;
-  
-  // handling server lists 
+
+  // handling server lists
   private List<Server> serverList = null;
   private boolean downloading = false;
   private boolean downloaded = false;
-  
-  
+
+  // pratial implementation of Parcelable interface
+  // this enables basic version of server environment to be passed between
+  // intents
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    dest.writeInt(automatic ? 1 : 0);
+    dest.writeString(branchName);
+    dest.writeInt(id);
+    dest.writeString(name);
+    dest.writeInt(repositoryId);
+  }
+
+  public ServerEnvironment(Parcel in) {
+    this.automatic = in.readInt() == 0 ? false : true;
+    this.branchName = in.readString();
+    this.id = in.readInt();
+    this.name = in.readString();
+    this.repositoryId = in.readInt();
+  }
+
+  @Override
+  public int describeContents() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public static final Parcelable.Creator<ServerEnvironment> CREATOR = new Parcelable.Creator<ServerEnvironment>() {
+    public ServerEnvironment createFromParcel(Parcel in) {
+      return new ServerEnvironment(in);
+    }
+
+    public ServerEnvironment[] newArray(int size) {
+      return new ServerEnvironment[size];
+    }
+  };
+
   public int getAccountId() {
     return accountId;
   }
+
   public void setAccountId(int accountId) {
     this.accountId = accountId;
   }
+
   public boolean isAutomatic() {
     return automatic;
   }
+
   public void setAutomatic(boolean automatic) {
     this.automatic = automatic;
   }
+
   public String getBranchName() {
     return branchName;
   }
+
   public void setBranchName(String branchName) {
     this.branchName = branchName;
   }
+
   public long getCreatedAt() {
     return createdAt;
   }
+
   public void setCreatedAt(String date) throws ParseException {
     this.createdAt = FORMATTER.parse(date.trim()).getTime();
   }
+
   public String getCurrentVersion() {
     return currentVersion;
   }
+
   public void setCurrentVersion(String currentVersion) {
     this.currentVersion = currentVersion;
   }
+
   public int getId() {
     return id;
   }
+
   public void setId(int id) {
     this.id = id;
   }
+
   public String getName() {
     return name;
   }
+
   public void setName(String name) {
     this.name = name;
   }
+
   public int getRepositoryId() {
     return repositoryId;
   }
+
   public void setRepositoryId(int repositoryId) {
     this.repositoryId = repositoryId;
   }
+
   public long getUpdatedAt() {
     return updatedAt;
   }
+
   public void setUpdatedAt(String date) throws ParseException {
     this.updatedAt = FORMATTER.parse(date.trim()).getTime();
   }
+
   public void setDownloading(boolean downloading) {
     this.downloading = downloading;
   }
+
   public boolean isDownloading() {
     return downloading;
   }
+
   public void setServerList(List<Server> serverList) {
     this.serverList = serverList;
   }
+
   public List<Server> getServerList() {
     return serverList;
   }
+
   public void setDownloaded(boolean downloaded) {
     this.downloaded = downloaded;
   }
+
   public boolean isDownloaded() {
     return downloaded;
   }
-  
-  
 
 }
