474636a47d365dbbfd778cf72c0964536112bdef
Bartek Filipowicz <bartek.f@applicake.com>
2011-07-19 16:45:04 +0200

    improves SharedPreferencesMockContext

--- Beanstalk Android ClientTest/src/com/applicake/beanstalkclient/test/SharedPreferencesMockContext.java
+++ Beanstalk Android ClientTest/src/com/applicake/beanstalkclient/test/SharedPreferencesMockContext.java
@@ -1,25 +1,40 @@
 package com.applicake.beanstalkclient.test;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.test.RenamingDelegatingContext;
 import android.test.mock.MockContext;
 
-
-
 public class SharedPreferencesMockContext extends MockContext {
-  private static final String TAG = "MyMockContext";
 
-  private static final String MOCK_PREFIX = "test.";
+  private static final String PREFIX = "test.";
 
-  
-  
-  
-  @Override
-  public SharedPreferences getSharedPreferences(String name, int mode) {
-    // TODO Auto-generated method stub
-    return super.getSharedPreferences(name, mode);
+  private Context mBaseContext;
+
+  public SharedPreferencesMockContext(Context context) {
+    mBaseContext = context;
   }
 
+  @Override
+  public String getPackageName() {
+    return mBaseContext.getPackageName();
+  }
+
+  @Override
+  public SharedPreferences getSharedPreferences(String name, int mode) {
+    return mBaseContext.getSharedPreferences(PREFIX + name, mode);
+  }
+
+  @Override
+  public ContentResolver getContentResolver() {
+    // TODO Auto-generated method stub
+    return mBaseContext.getContentResolver();
+  }
+
+  @Override
+  public Object getSystemService(String name) {
+    // TODO Auto-generated method stub
+    return mBaseContext.getSystemService(name);
+  }
 
 }
