9b9107631f93564ec888defcc9a8f14a201c64f7
Arek Olek <arek@applicake.com>
2011-08-01 18:31:17 +0200

    basic layout for release details

--- Beanstalk Android Client/src/com/applicake/beanstalkclient/activities/ReleaseDetailsActivity.java
+++ Beanstalk Android Client/src/com/applicake/beanstalkclient/activities/ReleaseDetailsActivity.java
@@ -1,11 +1,14 @@
 package com.applicake.beanstalkclient.activities;
 
+import com.applicake.beanstalkclient.R;
+
 import android.os.Bundle;
 
 public class ReleaseDetailsActivity extends BeanstalkActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    // TODO Auto-generated method stub
     super.onCreate(savedInstanceState);
+    
+    setContentView(R.layout.release_details_layout);
   }
 }
